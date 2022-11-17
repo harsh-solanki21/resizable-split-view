d66a194769e38c496858179925b858c862343b7c
Harsh Solanki <harsh.solanki2105@gmail.com>
2022-11-17 18:44:28 +0530

    final commit

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const eventController = require('../controllers/eventController')
 
-router.get('/', eventController.getData)
+router.get('/get', eventController.getData)
 
 router.post('/', eventController.addData)
 
